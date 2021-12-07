52c4409248e973fe186d7792f69c3a4f9803d30a
Mislav Dominovic <mislav.dominovic@tttech-auto.com>
2021-12-08 00:57:49 +0100

    Change dir of app in azure pipelines file

--- commands.sh
+++ commands.sh
@@ -6,9 +6,9 @@ python -m venv project_2_nd_venv
 . project_2_nd_venv/bin/activate
 make all
 
-az webapp up --sku F1 -n flaskmlwebapp
+az webapp up --sku F1 -n flaskml1337
 
-az webapp log tail
+az webapp log tail --name flaskml1337
 
 pip install locust
 locust
